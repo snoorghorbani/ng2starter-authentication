28b1503df65e8ffa7aa182f55e7bde623fe5ce8d
soushians noorghorbani <snoorghorbani@gmail.com>
2018-02-14 06:35:22 +0330

    1.0.40

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export { CaptchaComponent } from "./dump-components";
+export { SignInActionTypes, SignoutAction, AuthenticationActionTypes } from './actions';
+export * from "./authentication.config";
+export * from "./reducers";
+export * from "./authentication.module";
+export * from "./services";
